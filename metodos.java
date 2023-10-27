64df6ab3d49dc7d4ee0a36d09ec73c08bb1f8821
Pedro Matsushita <137213776+Pedro-Matsushita@users.noreply.github.com>
2023-10-27 12:48:03 +0000

    Mudança 27/10

--- metodos.java
+++ metodos.java
@@ -3,27 +3,33 @@ public class metodos
 {
 static Scanner teclado = new Scanner(System.in);
 public static void main(String[] args){
-    double vet[] = new double[15];
+    int vet[] = new int[5];
     int procurado = 10;
     int tamanho = vet.length;
 
     entradaValores(vet);
 
-    int buscarNumero = buscaSequencial(vet, procurado, n);
+    int encontraMaiorouMenor[] = metodoBolha(vet);
+
+   //int buscarNumero = buscaSequencial(vet, procurado, n);
+
+   exibeVetor(vet);
 }
-public static void entradaValores(double vet[]){
+public static void entradaValores(int vet[]){
+    Random r = new Random();
     for(int i = 0; i<vet.length; i++){
         System.out.print("Digite o valor do vetor: ");
-        vet[i] = teclado.nextDouble();
+        vet[i] = r.nextInt(15);
     }
     System.out.println();
 }
-//Método bolha encontrar o maior ou menor número
-/*public static int[] metodoBolha(double vet[]){
+//Método bolha encontrar o maior ou menor número e colocar em ordem
+//crescente ou decrescente
+public static int[] metodoBolha(int vet[]){
     int tamanho = vet.length;
     while(tamanho >= 2){
         for(int i = 0; i < tamanho-1; i++){
-            if(vet[i] > vet[i+1]){
+            if(vet[i] > vet[i+1]){ //Ordem crescente
                 int temp = vet[i];
                 vet[i] = vet[i+1];
                 vet[i+1] = temp;
@@ -32,8 +38,8 @@ public static void entradaValores(double vet[]){
         tamanho = tamanho - 1;
     }
     return vet;
-}*/
-public static int buscaSequencial(double vet[], double procurado, int tamanho){
+}
+/*public static int buscaSequencial(double vet[], double procurado, int tamanho){
     boolean achou = false;
     int i;
 
@@ -41,6 +47,10 @@ public static int buscaSequencial(double vet[], double procurado, int tamanho){
         if(vet[i] == procurado){
             achou = true;
             System.out.println("Número "+ procurado + " encontrado na posição "+ (i+1));
+            break;
+        }
+        else if(vet[i] > procurado){
+            System.out.println("Elemento não encontrado.");
             break;
         }
     }
@@ -51,5 +61,11 @@ public static int buscaSequencial(double vet[], double procurado, int tamanho){
     else{
         return -1; // retorna -1 indicando que não achou a chave
     }
+}*/
+public static void exibeVetor(int vet[]){
+    for(int i = 0; i < vet.length; i++){
+        System.out.println(vet[i]);
+    }
+    System.out.println();
 }
 }
